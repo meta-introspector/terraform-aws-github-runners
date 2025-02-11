d60003851833760337b2bd4a1651c32cb827681f
mike dupont <mike.dupont@introspector.local>
2025-02-11 15:30:08 -0500

    my example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -4,18 +4,21 @@ module "github_runner" {
   # Required parameters
   ############################
   # Naming for all created resources
-  name            = "github-runner-codebuild-test"
-  source_location = "https://github.com/my-org/my-repo.git"
+  name            = "github-runner-codebuild-eliza-build"
+  source_location = "https://github.com/meta-introspector/cloud-deployment-eliza"
 
   # Environment image is not specified so it will default to:
   # "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
+  environment_image = "public.ecr.aws/moia-oss/codebuild-ubuntu:jammy"
 
+  environment_type = "ARM_CONTAINER"
+    
   # Optional parameters
   ############################
-  description = "Created by my-org/my-runner-repo.git"
+  description = "Automated build"
 
-  github_personal_access_token = "example"
+#  github_personal_access_token = "example"
 
-  vpc_id     = "vpc-0ffaabbcc1122"
-  subnet_ids = ["subnet-0123", "subnet-0456"]
+#  vpc_id     = "vpc-0ffaabbcc1122"
+#  subnet_ids = ["subnet-0123", "subnet-0456"]
 }
